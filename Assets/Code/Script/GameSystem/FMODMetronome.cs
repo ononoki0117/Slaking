27b297475ddd49936abea09a8c04ea7b38f1a93a
ononoki0117 <tytttyty@naver.com>
2024-02-07 13:32:31 +0900

    24.02.07.

--- Assets/Code/Script/GameSystem/FMODMetronome.cs
+++ Assets/Code/Script/GameSystem/FMODMetronome.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class FMODMetronome : MonoBehaviour
 {
+    public int time;
+    public int startDelay;
+
+    uint GetTime()
+    {
+        AudioManager.instance.musicChannel.getPosition(out uint pos, FMOD.TIMEUNIT.MS);
+        return pos;
+    }
+    
+    public void SetMetronome()
+    {
+        int preBar = 10;
+        int barTime = time - startDelay;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
