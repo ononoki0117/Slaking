f082c0f097151fc20e052d25137e0614d28c58c6
ononoki0117 <tytttyty@naver.com>
2024-03-26 02:21:56 +0900

    web cam added

--- Assets/Code/Script/Util/WebcamTest.cs
+++ Assets/Code/Script/Util/WebcamTest.cs
@@ -3,10 +3,8 @@ using UnityEngine.UI;
 
 public class WebcamTest : MonoBehaviour
 {
-    public RawImage display1;
-    public RawImage display2;
-    WebCamTexture camTexture1;
-    WebCamTexture camTexture2;
+    public RawImage display;
+    WebCamTexture camTexture;
     private int currentIndex = 0;
 
     private void Start()
@@ -18,28 +16,16 @@ public class WebcamTest : MonoBehaviour
             Debug.Log(devices[i].name);
         }
 
-        if (camTexture1 != null)
+        if (camTexture != null)
         {
-            display1.texture = null;
-            camTexture1.Stop();
-            camTexture1 = null;
+            display.texture = null;
+            camTexture.Stop();
+            camTexture = null;
         }
         WebCamDevice device = WebCamTexture.devices[currentIndex];
-        camTexture1 = new WebCamTexture(device.name);
-        display1.texture = camTexture1;
-        camTexture1.Play();
-
-
-        if (camTexture2 != null)
-        {
-            display2.texture = null;
-            camTexture2.Stop();
-            camTexture2 = null;
-        }
-        WebCamDevice device2 = WebCamTexture.devices[currentIndex+1];
-        camTexture2 = new WebCamTexture(device2 .name);
-        display2.texture = camTexture2;
-        camTexture2.Play();
+        camTexture = new WebCamTexture(device.name);
+        display.texture = camTexture;
+        camTexture.Play();
     }
 
     // Update is called once per frame
